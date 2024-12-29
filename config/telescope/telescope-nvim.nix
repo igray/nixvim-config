2a1087d7f9921b89aa4747d3053e512eeb201847
Iain Gray <igray78756@gmail.com>
2024-12-28 20:08:45 -0600

    Giving up on previewing images in telescope

--- config/telescope/telescope-nvim.nix
+++ config/telescope/telescope-nvim.nix
@@ -4,26 +4,6 @@
     telescope-nvim.enable = lib.mkEnableOption "Enable telescope-nvim module";
   };
   config = lib.mkIf config.telescope-nvim.enable {
-    # extraPlugins = with pkgs.vimPlugins; [
-    #   telescope-file-browser-nvim
-    #   (pkgs.vimUtils.buildVimPlugin {
-    #     name = "telescope-media.nvim";
-    #     src = pkgs.fetchFromGitHub {
-    #       owner = "dharmx";
-    #       repo = "telescope-media.nvim";
-    #       rev = "aa67b00b3a0811c10c9c545127020d0fed04fc7e";
-    #       hash = "sha256-8nqw/Ce87jQiwLfCpaT5/8mwZE6259fLvX7bWhzBsFk=";
-    #     };
-    #   })
-    # ];
-    # extraConfigLua = ''
-    #   require("telescope").load_extension("media")
-    # '';
-    # extraPackages = with pkgs; [
-    #   fontforge
-    #   imagemagick
-    #   poppler_utils
-    # ];
     plugins.telescope = {
       enable = true;
       extensions = {
@@ -91,14 +71,6 @@
         pickers = {
           colorscheme = {
             enable_preview = true;
-          };
-          # find_files = {
-          #   theme = "ivy";
-          # };
-        };
-        extensions = {
-          media = {
-            backend = "viu";
           };
         };
       };
