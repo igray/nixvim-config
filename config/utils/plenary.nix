3aa54c1910ec4d1837e27e25501558b6cbd4e689
Iain Gray <igray78756@gmail.com>
2025-06-01 12:23:02 -0500

    Temporary debug statement

--- config/utils/plenary.nix
+++ config/utils/plenary.nix
@@ -15,6 +15,19 @@
     extraConfigLua = ''
       -- Force plenary to use system curl
       vim.g.plenary_curl_bin_path = "${pkgs.curl}/bin/curl"
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          -- Override plenary's curl function to see the actual command
+          local Job = require('plenary.job')
+          local original_new = Job.new
+          Job.new = function(opts)
+            if opts.command == 'curl' then
+              vim.notify("Curl command:", vim.inspect(opts))
+            end
+            return original_new(opts)
+          end
+        end,
+      })
     '';
   };
 }
