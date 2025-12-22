423c9e1b9d2608c192ab3a9a0229ee439022fa29
Iain Gray <igray78756@gmail.com>
2025-12-22 11:30:41 -0600

    Reconfigured grammars in treesitter

--- config/languages/treesitter-nvim.nix
+++ config/languages/treesitter-nvim.nix
@@ -1,133 +1,133 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   options = {
     treesitter-nvim.enable = lib.mkEnableOption "Enable treesitter-nvim module";
   };
   config = lib.mkIf config.treesitter-nvim.enable {
-    plugins.treesitter = {
-      enable = true;
-      settings = {
-        highlight = {
-          enable = true;
-        };
-        indent = {
-          enable = true;
-        };
-        autopairs = {
-          enable = true;
-        };
-        folding = {
-          enable = true;
-        };
-        ensure_installed = [
-          "bash"
-          "c"
-          "html"
-          "css"
-          "dockerfile"
-          "elm"
-          "gleam"
-          "html"
-          "javascript"
-          "jsdoc"
-          "json"
-          "ledger"
-          "lua"
-          "luadoc"
-          "luap"
-          "nix"
-          "rust"
-          "java"
-          "markdown"
-          "markdown_inline"
-          "python"
-          "query"
-          "regex"
-          "ruby"
-          "scss"
-          "tsx"
-          "typescript"
-          "vim"
-          "vimdoc"
-          "toml"
-          "yaml"
+    plugins = {
+      treesitter = {
+        enable = true;
+        grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+          bash
+          c
+          html
+          css
+          dockerfile
+          elm
+          gleam
+          html
+          javascript
+          jsdoc
+          json
+          ledger
+          lua
+          luadoc
+          luap
+          nix
+          rust
+          java
+          markdown
+          markdown_inline
+          python
+          query
+          regex
+          ruby
+          scss
+          tsx
+          typescript
+          vim
+          vimdoc
+          toml
+          yaml
         ];
-        auto_install = true;
-        incremental_selection = {
-          enable = true;
-          keymaps = {
-            init_selection = "<C-space>";
-            node_incremental = "<C-space>";
-            scope_incremental = false;
-            node_decremental = "<bs>";
+        highlight.enable = true;
+        indent.enable = true;
+        folding.enable = true;
+        settings = {
+          autopairs = {
+            enable = true;
+          };
+          incremental_selection = {
+            enable = true;
+            keymaps = {
+              init_selection = "<C-space>";
+              node_incremental = "<C-space>";
+              scope_incremental = false;
+              node_decremental = "<bs>";
+            };
+          };
+        };
+        nixvimInjections = true;
+      };
+
+      treesitter-textobjects = {
+        enable = true;
+        settings = {
+          select = {
+            enable = true;
+            lookahead = true;
+            keymaps = {
+              "aa" = "@parameter.outer";
+              "ia" = "@parameter.inner";
+              "af" = "@function.outer";
+              "if" = "@function.inner";
+              "ac" = "@class.outer";
+              "ic" = "@class.inner";
+              "ii" = "@conditional.inner";
+              "ai" = "@conditional.outer";
+              "il" = "@loop.inner";
+              "al" = "@loop.outer";
+              "at" = "@comment.outer";
+            };
+          };
+          move = {
+            enable = true;
+            gotoNextStart = {
+              "]m" = "@function.outer";
+              "]]" = "@class.outer";
+            };
+            gotoNextEnd = {
+              "]M" = "@function.outer";
+              "][" = "@class.outer";
+            };
+            gotoPreviousStart = {
+              "[m" = "@function.outer";
+              "[[" = "@class.outer";
+            };
+            gotoPreviousEnd = {
+              "[M" = "@function.outer";
+              "[]" = "@class.outer";
+            };
+          };
+          swap = {
+            enable = true;
+            swapNext = {
+              "<leader>a" = "@parameters.inner";
+            };
+            swapPrevious = {
+              "<leader>A" = "@parameter.outer";
+            };
           };
         };
       };
-      nixvimInjections = true;
-    };
 
-    plugins.treesitter-textobjects = {
-      enable = true;
-      settings = {
-        select = {
-          enable = true;
-          lookahead = true;
-          keymaps = {
-            "aa" = "@parameter.outer";
-            "ia" = "@parameter.inner";
-            "af" = "@function.outer";
-            "if" = "@function.inner";
-            "ac" = "@class.outer";
-            "ic" = "@class.inner";
-            "ii" = "@conditional.inner";
-            "ai" = "@conditional.outer";
-            "il" = "@loop.inner";
-            "al" = "@loop.outer";
-            "at" = "@comment.outer";
-          };
-        };
-        move = {
-          enable = true;
-          gotoNextStart = {
-            "]m" = "@function.outer";
-            "]]" = "@class.outer";
-          };
-          gotoNextEnd = {
-            "]M" = "@function.outer";
-            "][" = "@class.outer";
-          };
-          gotoPreviousStart = {
-            "[m" = "@function.outer";
-            "[[" = "@class.outer";
-          };
-          gotoPreviousEnd = {
-            "[M" = "@function.outer";
-            "[]" = "@class.outer";
-          };
-        };
-        swap = {
-          enable = true;
-          swapNext = {
-            "<leader>a" = "@parameters.inner";
-          };
-          swapPrevious = {
-            "<leader>A" = "@parameter.outer";
-          };
-        };
+      ts-autotag = {
+        enable = true;
       };
-    };
 
-    plugins.ts-autotag = {
-      enable = true;
-    };
+      treesitter-context = {
+        enable = true;
+      };
 
-    plugins.treesitter-context = {
-      enable = true;
-    };
-
-    plugins.ts-context-commentstring = {
-      enable = true;
-      disableAutoInitialization = false;
+      ts-context-commentstring = {
+        enable = true;
+        disableAutoInitialization = false;
+      };
     };
   };
 }
